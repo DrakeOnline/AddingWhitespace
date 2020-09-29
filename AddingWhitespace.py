00226bae99f061cb05493adffeba5f68fcf28437
DrakeOnline <drakec1997@gmail.com>
2020-09-29 13:57:10 -0700

    Add mechanic for loading database into RAM

--- AddingWhitespace.py
+++ AddingWhitespace.py
@@ -9,3 +9,21 @@
 #-------------------------------------------------------------------------------|
 #   Time Spent Programming: 0 hour(s) 0 minute(s) 0 second(s)                   |
 #-------------------------------------------------------------------------------|
+
+
+import json
+from os import system
+
+
+# Load words from json file
+wordDatabase    = open('words_dictionary.json')
+dictionary      = []
+wordCount       = 370100
+currentCount    = 0
+
+for word in json.load(wordDatabase):
+    dictionary.append(word)
+    currentCount += 1
+    # Print percentage of words laoded
+    print(f"\r{int((currentCount/wordCount)*100)}% of words loaded", end="")
+print()
