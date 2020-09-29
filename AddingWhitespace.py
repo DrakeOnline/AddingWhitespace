550c4d62aed7a5c9c08e19a7945ab503e6feb73e
DrakeOnline <drakec1997@gmail.com>
2020-09-29 15:09:21 -0700

    Added mechanism that adds space after all punctuation

--- AddingWhitespace.py
+++ AddingWhitespace.py
@@ -7,12 +7,11 @@
 #   Last Updated:   September 29th, 2020                                        |
 #   Condition:      In progress                                                 |
 #-------------------------------------------------------------------------------|
-#   Time Spent Programming: 0 hour(s) 49 minute(s) 27 second(s)                 |
+#   Time Spent Programming: 1 hour(s) 20 minute(s) 58 second(s)                 |
 #-------------------------------------------------------------------------------|
 
 
 import json
-from os import system
 
 
 # Load words from json file
@@ -21,9 +20,34 @@ dictionary      = []
 wordCount       = 370100
 currentCount    = 0
 
+# load each word in the databse into a list for quicker checking
 for word in json.load(wordDatabase):
     dictionary.append(word)
     currentCount += 1
     # Print percentage of words laoded
     print(f"\r{int((currentCount/wordCount)*100)}% of words loaded", end="")
 print()
+
+
+# Add spaces after every punctuation
+punctuation     = ['.', '!', '?', ',', ':', ';']
+originalFile    = open('Newtext.txt','r')
+newString       = ""
+
+# Get each line
+for line in originalFile:
+
+    # Get each character in each line
+    for character in line:
+        newString += character
+
+        # Check each character in the line with each punctuation mark
+        for mark in punctuation:
+            if character == mark:
+                newString += " "
+
+
+# Write string with new spacing to temporary file
+newFile = open('tmp.txt', 'w')
+newFile.write(newString)
+newFile.close()
