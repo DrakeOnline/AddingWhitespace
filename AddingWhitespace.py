1702208220147a9b2be17e5a2f9ae905e1a85c01
Drake <drakec1997@gmail.com>
2020-09-29 12:45:11 -0700

    Initial commit

---
+++ AddingWhitespace.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------|
+#   Title:          AddingWhitespace                                            |
+#   Author:         Drake G. Cummings                                           |
+#   Purpose:        Add correct whitespace to a file                            |
+#-------------------------------------------------------------------------------|
+#   Started:        September 29th, 2020                                        |
+#   Last Updated:   September 29th, 2020                                        |
+#   Condition:      In progress                                                 |
+#-------------------------------------------------------------------------------|
+#   Time Spent Programming: 0 hour(s) 0 minute(s) 0 second(s)                   |
+#-------------------------------------------------------------------------------|
